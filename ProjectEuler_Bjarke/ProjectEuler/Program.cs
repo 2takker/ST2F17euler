2884ba6b23f74da8affb1fb2370e7da9622a69d4
Bjarke Hansen <2takker@gmail.com>
2017-04-24 10:09:32 +0200

    projectEuler_ini_Bjarke_
    Initial commit

---
+++ ProjectEuler_Bjarke/ProjectEuler/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static long sumMultiple3t5(int limit)
+        {
+            long result = 0;
+
+            for (int i = 1; i < limit; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    result += i;
+                }
+            }
+
+            return result;
+        }
+
+        static long sumEvenFib(int limit)
+        {
+            long result = 0;
+            List<long> fibList = new List<long>();
+
+            for(int i = 1; i < limit; i++ )
+            {
+                
+                if(((i-1)+i)%2 == 0 && )
+
+            }
+
+            return result;
+        }
+    }
+
+}
